76801b52e75df8ad51f23dd0bd010822324b91cf
Thang7804 <thang7804hq@gmail.com>
2024-10-09 00:03:57 +0700

    Add files via upload

---
+++ src/Employee.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Employee {
+    private int employeeID;   
+    private String name;      
+    private String dob;       
+    private String gender;     
+    private String phoneNumber; 
+    private String address;    
+    private String position;   
+    private double salary;     
+    private String shift;
+public void add(Employee employee) {
+}
+
+public void update(String employeeID, Employee updatedEmployee) {
+  
+}
+
+public void remove(String employeeID) {   
+}
+}
+
+
+
+
