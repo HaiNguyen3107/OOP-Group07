8c7ae89f51c64a287c03c05d5825bf5eaa03ec56
HaiNguyen3107 <hainguyenf07@gmail.com>
2024-10-09 00:36:28 +0700

    Update Price.java

--- src/Price.java
+++ src/Price.java
@@ -1,1 +1,18 @@
+public class Price {
+    private String pricingID;
+    private String vehicleType;
+    private double HourlyRate;
+    private double DailyRate;
+    private double MonthlyRate;
 
+    public double getPricing(Vehicle type){
+
+    }
+    public void setPricing(Vehicle type, double HourlyRate, double DailyRate, double MonthlyRate){
+
+    }
+    public double calculateTotalPrice(Vehicle Type, Vehicle entryTime, Vehicle exitTime, double HourlyRate, double DailyRate, double MonthlyRate){
+        
+    }
+
+}
