6faa874ad1ef598bff61023fe7312aede691f3c7
HaiNguyen3107 <hainguyenf07@gmail.com>
2024-10-09 00:16:41 +0700

    Update and rename Data to ParkingSlot.java

---
+++ src/ParkingSlot.java
@@ -0,0 +1,15 @@
+public class ParkingSlot {
+    private String id;
+    private Boolean isOccupied;
+    private String type;
+    private Vehicle vehicle;
+    public Boolean getStatus(){
+        
+    }
+    public void parkVehicle(Vehicle vehicle){
+        
+    }
+    public void removeVehicle(){
+
+    }
+}
