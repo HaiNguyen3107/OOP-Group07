a133bce328edf2abcb96a03f38d06416bd096346
HaiNguyen3107 <hainguyenf07@gmail.com>
2024-10-09 00:22:19 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,5 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ParkingLot parkingLot = new ParkingLot(10); 
     }
 }
