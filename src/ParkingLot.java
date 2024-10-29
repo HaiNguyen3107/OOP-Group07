693bef5aabe1c96288b9f05a4d9dc176b34a98be
HaiNguyen3107 <hainguyenf07@gmail.com>
2024-10-29 22:12:35 +0700

    Update ParkingLot.java

--- src/ParkingLot.java
+++ src/ParkingLot.java
@@ -6,31 +6,21 @@ import java.util.List;
 public class ParkingLot {
     private List<Vehicle> parkedVehicles;
     private int totalSpaces;
-    private int occupiedSpaces;
-
-    
+    private int occupiedSpaces;    
     public ParkingLot(int totalSpaces) {
         this.totalSpaces = totalSpaces;
         this.parkedVehicles = new ArrayList<>();
         this.occupiedSpaces = 0;
     }
-
-    
     public void parkVehicle(Vehicle vehicle) {
         
     }
-
-    
     public void unparkVehicle(String licensePlate) {
        
     }
-
-   
     public void displayParkedVehicles() {
       
     }
-
-   
     public int getAvailableSpaces() {
         return totalSpaces - occupiedSpaces;
     }
