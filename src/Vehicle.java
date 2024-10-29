57a47c259db3fad45ba20da9c9b7abdac37027fd
HaiNguyen3107 <hainguyenf07@gmail.com>
2024-10-29 22:11:49 +0700

    Update Vehicle.java

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,23 +1,45 @@
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+
 public class Vehicle {
-    private String vehicleID;
-    private String vehicleType;
     private String licensePlate;
-    private Date entryTime;
-    private Date exitTime;
-
-    public String getVehicleID() {
-        return vehicleID;
+    private String vehicleType; 
+    private LocalDateTime entryTime;
+    private LocalDateTime exitTime;
+    private double parkingFee;
+    public Vehicle(String licensePlate, String vehicleType, LocalDateTime entryTime) {
+        this.licensePlate = licensePlate;
+        this.vehicleType = vehicleType;
+        this.entryTime = entryTime;
     }
-
+    public String getLicensePlate() {
+        return licensePlate;
+    }
     public String getVehicleType() {
         return vehicleType;
     }
-    public void setEntryTime(Date entryTime) {
+    public LocalDateTime getEntryTime() {
+        return entryTime;
+    }
+    public void setEntryTime(LocalDateTime entryTime) {
         this.entryTime = entryTime;
     }
-
-    public void setExitTime(Date exitTime) {
+    public void setExitTime(LocalDateTime exitTime) {
         this.exitTime = exitTime;
-    } 
-
+        calculateParkingFee();
+    }
+    public LocalDateTime getExitTime() {
+        return exitTime;
+    }
+    public double getParkingFee() {
+        return parkingFee;
+    }
+    protected void calculateParkingFee() {
+        
+    }
+    @Override
+    public String toString() {
+        
+    }
 }
