69cd02213ee1d1d36b3068195927f563b135f5fe
HaiNguyen3107 <hainguyenf07@gmail.com>
2024-10-29 22:09:40 +0700

    Update ParkingLot.java

--- src/ParkingLot.java
+++ src/ParkingLot.java
@@ -1,15 +1,37 @@
-public class ParkingSlot {
-    private String id;
-    private Boolean isOccupied;
-    private String type;
-    private Vehicle vehicle;
-    public Boolean getStatus(){
-        
-    }
-    public void parkVehicle(Vehicle vehicle){
-        
-    }
-    public void removeVehicle(){
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
+
+public class ParkingLot {
+    private List<Vehicle> parkedVehicles;
+    private int totalSpaces;
+    private int occupiedSpaces;
+
+    
+    public ParkingLot(int totalSpaces) {
+        this.totalSpaces = totalSpaces;
+        this.parkedVehicles = new ArrayList<>();
+        this.occupiedSpaces = 0;
+    }
+
+    
+    public void parkVehicle(Vehicle vehicle) {
+        
+    }
+
+    
+    public void unparkVehicle(String licensePlate) {
+       
+    }
+
+   
+    public void displayParkedVehicles() {
+      
+    }
+
+   
+    public int getAvailableSpaces() {
+        return totalSpaces - occupiedSpaces;
     }
 }
