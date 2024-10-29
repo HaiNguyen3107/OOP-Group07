b205cf2e2654db20587f539f32f0b9fd7a669020
HaiNguyen3107 <hainguyenf07@gmail.com>
2024-10-29 22:12:15 +0700

    Update Ticket.java

--- src/Ticket.java
+++ src/Ticket.java
@@ -7,28 +7,20 @@ public class Ticket extends Vehicle {
     private boolean isMonthlyTicket;
     private LocalDateTime monthlyTicketExpiryDate;
     private static List<Ticket> tickets = new ArrayList<>();
-
-    
     public Ticket(String licensePlate, String vehicleType, LocalDateTime entryTime, boolean isMonthlyTicket, LocalDateTime expiryDate) {
       
     }
 
-    
     private void registerMonthlyTicket() {
         
     }
-
-    
     @Override
     protected void calculateParkingFee() {
      
     }
-
-    
     public static void displayAllTickets() {
        
     }
-
     @Override
     public String toString() {
         
