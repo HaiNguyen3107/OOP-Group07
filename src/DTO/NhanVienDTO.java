46152311004e2a0172174b9a7bfc9b9d365f031e
HaiNguyen3107 <hainguyenf07@gmail.com>
2024-12-02 10:27:58 +0700

    Add files via upload

---
+++ src/DTO/NhanVienDTO.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DTO;
+
+/**
+ *
+ * @author ASUS
+ */
+public class NhanVienDTO {
+    private String cccd, tenNhanVien, soDienThoai, taiKhoan, matKhau;
+
+    public NhanVienDTO() {
+    }
+
+    public NhanVienDTO(String cccd, String tenNhanVien, String soDienThoai, String taiKhoan, String matKhau) {
+        this.cccd = cccd;
+        this.tenNhanVien = tenNhanVien;
+        this.soDienThoai = soDienThoai;
+        this.taiKhoan = taiKhoan;
+        this.matKhau = matKhau;
+    }
+
+    public String getCccd() {
+        return cccd;
+    }
+
+    public void setCccd(String cccd) {
+        this.cccd = cccd;
+    }
+
+    public String getTenNhanVien() {
+        return tenNhanVien;
+    }
+
+    public void setTenNhanVien(String tenNhanVien) {
+        this.tenNhanVien = tenNhanVien;
+    }
+
+    public String getSoDienThoai() {
+        return soDienThoai;
+    }
+
+    public void setSoDienThoai(String soDienThoai) {
+        this.soDienThoai = soDienThoai;
+    }
+
+    public String getTaiKhoan() {
+        return taiKhoan;
+    }
+
+    public void setTaiKhoan(String taiKhoan) {
+        this.taiKhoan = taiKhoan;
+    }
+
+    public String getMatKhau() {
+        return matKhau;
+    }
+
+    public void setMatKhau(String matKhau) {
+        this.matKhau = matKhau;
+    }
+
+    
+}
